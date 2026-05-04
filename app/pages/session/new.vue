42a67931c921856f790ccb9f4056f2c19964f1f2
Oscar Recio <reciog.oscar@hotmail.com>
2026-05-04 17:03:37 -0600

    feat: hide future systems

--- app/pages/session/new.vue
+++ app/pages/session/new.vue
@@ -39,27 +39,27 @@
         'The Duality Dice system in the Daggerheart RPG uses two d12s (Hope and Fear) rolled together to determine success and narrative tone.',
       icon: 'i-lucide-dices'
     },
-    {
-      id: 'yze',
-      title: 'Year Zero Engine',
-      description:
-        'In the dice pool system, every action is resolved by rolling a bunch of d6 dice, only the results of 6 count as successes, and the number of successes determines the outcome of the action.',
-      icon: 'i-lucide-dices'
-    },
-    {
-      id: 'step',
-      title: 'Step Dice',
-      description:
-        'The step-dice variant uses different-sized dice (d6, d8, d10…), giving a gentler difficulty curve and fewer “push” penalties.',
-      icon: 'i-lucide-dices'
-    },
-    {
-      id: '2d20',
-      title: 'Modiphius 2D20',
-      description:
-        'The Modiphius 2D20 system uses two d20 dice to determine the outcome of actions, with the difference between the dice determining the degree of success.',
-      icon: 'i-lucide-dices'
-    }
+    // {
+    //   id: 'yze',
+    //   title: 'Year Zero Engine',
+    //   description:
+    //     'In the dice pool system, every action is resolved by rolling a bunch of d6 dice, only the results of 6 count as successes, and the number of successes determines the outcome of the action.',
+    //   icon: 'i-lucide-dices'
+    // },
+    // {
+    //   id: 'step',
+    //   title: 'Step Dice',
+    //   description:
+    //     'The step-dice variant uses different-sized dice (d6, d8, d10…), giving a gentler difficulty curve and fewer “push” penalties.',
+    //   icon: 'i-lucide-dices'
+    // },
+    // {
+    //   id: '2d20',
+    //   title: 'Modiphius 2D20',
+    //   description:
+    //     'The Modiphius 2D20 system uses two d20 dice to determine the outcome of actions, with the difference between the dice determining the degree of success.',
+    //   icon: 'i-lucide-dices'
+    // }
   ])
 
   function selectSystem(index: number, id: string) {
